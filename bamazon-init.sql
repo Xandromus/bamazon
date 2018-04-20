0714180dba70050076ead9fa890ca3f5743140b2
Xandromus <xanderrapstine@gmail.com>
2018-04-19 21:22:44 -0500

    changed the order of the tables in the init and seed files and added a foreign key constraint

--- bamazon-init.sql
+++ bamazon-init.sql
@@ -3,6 +3,16 @@ DROP SCHEMA IF EXISTS bamazon;
 CREATE SCHEMA `bamazon`;
 USE bamazon;
 
+CREATE TABLE `departments` (
+    `department_id` INTEGER(10) UNSIGNED AUTO_INCREMENT NOT NULL,
+    `department_name` VARCHAR(255) NOT NULL,
+    `over_head_costs` DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY(department_id)
+);
+
+CREATE INDEX department_name
+ON departments (department_name);
+
 CREATE TABLE `products`(
     `item_id` INTEGER(10) UNSIGNED AUTO_INCREMENT NOT NULL,
     `product_name` VARCHAR(255) NOT NULL,
@@ -10,15 +20,6 @@ CREATE TABLE `products`(
     `price` DECIMAL(10,2) NOT NULL,
     `stock_quantity` INTEGER(10) UNSIGNED NOT NULL,
     `product_sales` DECIMAL(10,2) UNSIGNED NOT NULL,
-    PRIMARY KEY(item_id)
-);
-
-CREATE INDEX department_name
-ON products (department_name);
-
-CREATE TABLE `departments` (
-    `department_id` INTEGER(10) UNSIGNED AUTO_INCREMENT NOT NULL,
-    `department_name` VARCHAR(255) NOT NULL,
-    `over_head_costs` DECIMAL(10,2) NOT NULL,
-    PRIMARY KEY(department_id)
+    PRIMARY KEY(item_id),
+    FOREIGN KEY(department_name) REFERENCES departments(department_name)
 );
--- bamazon-seed.sql
+++ bamazon-seed.sql
@@ -1,3 +1,13 @@
+INSERT INTO `departments` (`department_name`, `over_head_costs`)
+VALUES
+    ("Automotive", 5000),
+    ("Bedding", 4000),
+    ("Dishes", 1000),
+    ("Electronics", 10000),
+    ("Footwear", 5000),
+    ("Men's Clothing", 8000),
+    ("Toys", 5000);
+
 INSERT INTO `products` (`product_name`, `department_name`, `price`, `stock_quantity`, `product_sales`)
 VALUES
     ("Bose SoundLink Color Bluetooth Speaker", "Electronics", 129.00, 12, 0),
@@ -12,13 +22,3 @@ VALUES
     ("Bachmann Trains Thomas and Friends - James the Red Engine", "Toys", 55.96, 10, 0),
     ("AstroAI Digital Tire Pressure Gauge", "Automotive", 8.47, 13, 0),
     ("Tan Converse All-Star Low-Top Shoes", "Footwear", 49.99, 4, 0);
-
-INSERT INTO `departments` (`department_name`, `over_head_costs`)
-VALUES
-    ("Automotive", 5000),
-    ("Bedding", 4000),
-    ("Dishes", 1000),
-    ("Electronics", 10000),
-    ("Footwear", 5000),
-    ("Men's Clothing", 8000),
-    ("Toys", 5000);
