f540c60288effbdceaa3dfeec215673e0397f076
Xandromus <xanderrapstine@gmail.com>
2018-04-19 20:55:39 -0500

    finished up sql init and seed files

---
+++ bamazon-init.sql
@@ -0,0 +1,24 @@
+DROP SCHEMA IF EXISTS bamazon;
+
+CREATE SCHEMA `bamazon`;
+USE bamazon;
+
+CREATE TABLE `products`(
+    `item_id` INTEGER(10) UNSIGNED AUTO_INCREMENT NOT NULL,
+    `product_name` VARCHAR(255) NOT NULL,
+    `department_name` VARCHAR(255) NOT NULL,
+    `price` DECIMAL(10,2) NOT NULL,
+    `stock_quantity` INTEGER(10) UNSIGNED NOT NULL,
+    `product_sales` DECIMAL(10,2) UNSIGNED NOT NULL,
+    PRIMARY KEY(item_id)
+);
+
+CREATE INDEX department_name
+ON products (department_name);
+
+CREATE TABLE `departments` (
+    `department_id` INTEGER(10) UNSIGNED AUTO_INCREMENT NOT NULL,
+    `department_name` VARCHAR(255) NOT NULL,
+    `over_head_costs` DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY(department_id)
+);
---
+++ bamazon-seed.sql
@@ -0,0 +1,24 @@
+INSERT INTO `products` (`product_name`, `department_name`, `price`, `stock_quantity`, `product_sales`)
+VALUES
+    ("Bose SoundLink Color Bluetooth Speaker", "Electronics", 129.00, 12, 0),
+    ("Down Comforter", "Bedding", 25.00, 500, 0),
+    ("Lacoste Men's Short Sleeve Striped Polo Shirt", "Men's Clothing", 125.00, 150, 0),
+    ("Bone Chine Plate", "Dishes", 4.00, 4, 0),
+    ("LEGO Star Wars Yoda's Hut Building Kit", "Toys", 29.99, 135, 0),
+    ("Sony 55-Inch 4K Ultra HD Smart LED TV", "Electronics", 598.00, 30, 0),
+    ("Mpow Bluetooth Headphones", "Electronics", 35.99, 415, 0),
+    ("Penguin Men's Elastic Waist Swim Trunks", "Men's Clothing", 41.99, 80, 0),
+    ("Hankook DynaPro Off-Road Tire", "Automotive", 104.00, 101, 0),
+    ("Bachmann Trains Thomas and Friends - James the Red Engine", "Toys", 55.96, 10, 0),
+    ("AstroAI Digital Tire Pressure Gauge", "Automotive", 8.47, 13, 0),
+    ("Tan Converse All-Star Low-Top Shoes", "Footwear", 49.99, 4, 0);
+
+INSERT INTO `departments` (`department_name`, `over_head_costs`)
+VALUES
+    ("Automotive", 5000),
+    ("Bedding", 4000),
+    ("Dishes", 1000),
+    ("Electronics", 10000),
+    ("Footwear", 5000),
+    ("Men's Clothing", 8000),
+    ("Toys", 5000);
